5c8808b90a8658f576f3ecbf8b62ee41d24bcdc7
suumiizxc123 <suumiizxc@gmail.com>
2025-06-19 07:13:40 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  experimental: {
+    appDir: true,
+  },
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
